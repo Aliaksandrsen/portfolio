bae82233d2258a30ee6b42ecce7798a22fad1fc5
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-01-01 18:31:00 +0300

    feat: add new projects

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
-let coll = document.querySelector(".collapsible");
+const coll = document.querySelector('.collapsible');
+const coll2 = document.querySelector('.collapsible2');
+const coll3 = document.querySelector('.collapsible3');
+const coll4 = document.querySelector('.collapsible4');
+const coll5 = document.querySelector('.collapsible5');
 
-coll.addEventListener("click", function () {
-  this.classList.toggle("active");       // add class if no, delete if is
-  let content = this.nextElementSibling; // div after button
-  if (content.style.maxHeight) {
-    content.style.maxHeight = '';        // we need changin from 0 => maxHeight (we need it for transition )
-  } else {
-    content.style.maxHeight = content.scrollHeight + "px";
-  }
-});
-
-
-let coll2 = document.querySelector(".collapsible2");
-
-coll2.addEventListener("click", function () {
-  this.classList.toggle("active2");
-  let content = this.nextElementSibling;
+coll.addEventListener('click', function () {
+  this.classList.toggle('active');
+  const content = this.nextElementSibling;
   if (content.style.maxHeight) {
     content.style.maxHeight = '';
   } else {
-    content.style.maxHeight = content.scrollHeight + "px";
+    content.style.maxHeight = `${content.scrollHeight}px`;
   }
 });
 
 
-let coll3 = document.querySelector(".collapsible3");
-
-coll3.addEventListener("click", function () {
-  this.classList.toggle("active3");
-  let content = this.nextElementSibling;
+function cliker1() {
+  this.classList.toggle('active2');
+  const content = this.nextElementSibling;
   if (content.style.maxHeight) {
     content.style.maxHeight = '';
+    if (document.body.offsetWidth <= 768) {
+      this.parentElement.parentElement.querySelector('.sl__img').classList.remove('blur');
+    }
   } else {
-    content.style.maxHeight = content.scrollHeight + "px";
+    content.style.maxHeight = `${content.scrollHeight}px`;
+    if (document.body.offsetWidth <= 768) {
+      this.parentElement.parentElement.querySelector('.sl__img').classList.add('blur');
+    }
   }
-});
-
+}
+coll2.addEventListener('click', cliker1);
+coll3.addEventListener('click', cliker1);
+coll4.addEventListener('click', cliker1);
+coll5.addEventListener('click', cliker1);
 
 $('.sl').slick();
